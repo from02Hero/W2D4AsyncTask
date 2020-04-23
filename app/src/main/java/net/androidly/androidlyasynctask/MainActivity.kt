7d09e4103dc05fb089e154bb446507806ff9e128
srgarcia <srgarcia@enhanceit.com>
2020-04-23 09:50:29 -0400

    updated main activity

--- app/src/main/java/net/androidly/androidlyasynctask/MainActivity.kt
+++ app/src/main/java/net/androidly/androidlyasynctask/MainActivity.kt
@@ -1,80 +1,39 @@
 package net.androidly.androidlyasynctask
 
-import android.support.v7.app.AppCompatActivity
+import android.app.LoaderManager
+import android.content.Loader
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
-import android.os.AsyncTask
+import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.Toast
-import java.lang.ref.WeakReference
+import kotlinx.android.synthetic.main.activity_main.*
 
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<String> {
 
     var myVariable = 10
+    // Unique id for loader
+    private val LDR_BASIC_ID = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnDoAsync.setOnClickListener {
-            val task = MyAsyncTask(this)
-            task.execute(10)
+            loaderManager.initLoader(LDR_BASIC_ID, Bundle.EMPTY, this)
         }
     }
 
-    companion object {
-        class MyAsyncTask internal constructor(context: MainActivity) : AsyncTask<Int, String, String?>() {
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<String> {
+        progressBar.visibility = View.VISIBLE
+        return BasicLoader(this, 10)
+    }
 
-            private var resp: String? = null
-            private val activityReference: WeakReference<MainActivity> = WeakReference(context)
+    override fun onLoadFinished(loader: Loader<String>?, data: String?) {
+        progressBar.visibility = View.GONE
+        textView.text = data.let { it }
+        myVariable = 100
+    }
 
-            override fun onPreExecute() {
-                val activity = activityReference.get()
-                if (activity == null || activity.isFinishing) return
-                activity.progressBar.visibility = View.VISIBLE
-            }
-
-            override fun doInBackground(vararg params: Int?): String? {
-                publishProgress("Sleeping Started") // Calls onProgressUpdate()
-                try {
-                    val time = params[0]?.times(1000)
-                    time?.toLong()?.let { Thread.sleep(it / 2) }
-                    publishProgress("Half Time") // Calls onProgressUpdate()
-                    time?.toLong()?.let { Thread.sleep(it / 2) }
-                    publishProgress("Sleeping Over") // Calls onProgressUpdate()
-                    resp = "Android was sleeping for " + params[0] + " seconds"
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
-                    resp = e.message
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                    resp = e.message
-                }
-
-                return resp
-            }
-
-
-            override fun onPostExecute(result: String?) {
-
-                // execution of result of Long time consuming operation
-                val activity = activityReference.get()
-                if (activity == null || activity.isFinishing) return
-                // access Activity member variables or modify the activity's UI
-                activity.progressBar.visibility = View.GONE
-                activity.textView.text = result.let { it }
-                activity.myVariable = 100
-            }
-
-            override fun onProgressUpdate(vararg text: String?) {
-
-                val activity = activityReference.get()
-                if (activity == null || activity.isFinishing) return
-
-                Toast.makeText(activity, text.firstOrNull(), Toast.LENGTH_SHORT).show()
-
-            }
-        }
+    override fun onLoaderReset(loader: Loader<String>?) {
+        progressBar.visibility = View.VISIBLE
     }
 }
